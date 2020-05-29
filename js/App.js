5f98f5d8476ac5a6286d812290ed403e23cac147
YunaH <YunaH503@gmail.com>
2020-05-28 21:35:06 -0400

    Refactored App component to add App function

--- js/App.js
+++ js/App.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
-export default function App() {
+
+const App = props => {
+//   const [songs, setSongs] = useState([])
+//
+//   useEffect(() => {
+//     axios({
+//       url: `${apiUrl}/songs`,
+//       method: 'GET'
+//     })
+//       .then(res => setSongs(res.data.songs))
+//       .catch()
+//   }, [])
+
+
   return (
     <h2>The React Thing</h2>
-  );
+  )
 }
+
+export default App
